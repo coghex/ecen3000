ae6ae8d3b3ac0ed039a5a51211a6bdb19db51786
Vince Coghlan <vince.coghlan@colorado.edu>
2014-01-16 15:53:16 -0700

    initial commit

---
+++ lib_small_printf_m0/Lib/src/small_printf_support.d
@@ -0,0 +1,7 @@
+src/small_printf_support.d: ../src/small_printf_support.c \
+ /Users/vincecoghlan/Documents/LPCXpresso_6.1.2/workspace/lib_small_printf_m0/inc/small_printf.h \
+ /Users/vincecoghlan/Documents/LPCXpresso_6.1.2/workspace/lib_small_printf_m0/inc/small_utils.h
+
+/Users/vincecoghlan/Documents/LPCXpresso_6.1.2/workspace/lib_small_printf_m0/inc/small_printf.h:
+
+/Users/vincecoghlan/Documents/LPCXpresso_6.1.2/workspace/lib_small_printf_m0/inc/small_utils.h:
---
+++ lib_small_printf_m0/Lib/src/small_utils.d
@@ -0,0 +1,4 @@
+src/small_utils.d: ../src/small_utils.c \
+ /Users/vincecoghlan/Documents/LPCXpresso_6.1.2/workspace/lib_small_printf_m0/inc/small_utils.h
+
+/Users/vincecoghlan/Documents/LPCXpresso_6.1.2/workspace/lib_small_printf_m0/inc/small_utils.h:
